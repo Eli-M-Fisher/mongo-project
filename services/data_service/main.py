5ad7f6ee0732f64b655380a2e0fb46184305655c
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-21 08:50:33 +0300

    initialize structure for main

---
+++ services/data_service/main.py
@@ -0,0 +1,13 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class FilterCondition(BaseModel):
+
+
+
+class SortCondition(BaseModel):
+
+
+
+class SearchCriteria(BaseModel):
