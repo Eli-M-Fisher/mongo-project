2fbcfa7df071d062862e5de13c7a2dceb05af7d6
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-21 16:01:01 +0300

    finish updata commands

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -1,26 +1,33 @@
-REM Build Docker image
-docker build -t hostile-tweets-ex .
+REM i build docker image (local) with unique version tag
+set VERSION=v5
+docker buildx build --platform linux/amd64 -t hostile-tweets-ex:%VERSION% .
 
-REM Run locally with Mongo connection
-docker run -it --rm -p 8000:8000 -e MONGO_URI="mongodb+srv://IRGC:iraniraniran@iranmaldb.gurutam.mongodb.net/" hostile-tweets-ex
+REM now i can run locally for testing
+docker run -it --rm -p 8000:8000 ^
+  -e MONGO_URI="mongodb+srv://IRGC:iraniraniran@iranmaldb.gurutam.mongodb.net/" ^
+  hostile-tweets-ex:%VERSION%
 
-REM Tag for Docker Hub
-docker tag hostile-tweets-ex your_dockerhub_user/hostile-tweets-ex:latest
+REM tag image for docker hub
+docker tag hostile-tweets-ex:%VERSION% elif2/hostile-tweets-ex:%VERSION%
 
-REM Push to Docker Hub
-docker push your_dockerhub_user/hostile-tweets-ex:latest
+REM than i push to Docker Hub
+docker push elif2/hostile-tweets-ex:%VERSION%
 
-REM Clean previous project
+REM i did needed from time to time to clean previous deployment in openshift
 oc delete all --selector app=hostile-tweets-ex
 
-REM Apply secrets and configmap
+REM then apply secrets and configmap
 oc apply -f infra/secrets.yaml
 oc apply -f infra/configmap.yaml
 
-REM Deploy app
+REM deploy the app yaml (deployment, service, route)
 oc apply -f infra/deployment.yaml
 oc apply -f infra/service.yaml
 oc apply -f infra/route.yaml
 
-REM Get route URL
-oc get route hostile-tweets-ex-route -o jsonpath='{.spec.host}'
+REM 8. if deeded to udate deployment to use the new image
+oc set image deployment/hostile-tweets-ex-deployment hostile-tweets-ex=your_dockerhub_user/hostile-tweets-ex:%VERSION%
+
+REM that i got route URL
+oc get route hostile-tweets-ex-route -o jsonpath="{.spec.host}"
+echo.
